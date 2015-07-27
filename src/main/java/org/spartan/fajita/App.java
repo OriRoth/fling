ceb5d17e2b1262d8585eccef38c27e0d733fa04c
Yossi Gil <yossi.gil@gmail.com>
2015-07-27 09:48:55 +0300

    Some demo program to be mutilated by Tomer

--- src/main/java/org/spartan/fajita/App.java
+++ src/main/java/org/spartan/fajita/App.java
@@ -6,6 +6,6 @@ package org.spartan.fajita;
  */
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello,  World!");
   }
 }
