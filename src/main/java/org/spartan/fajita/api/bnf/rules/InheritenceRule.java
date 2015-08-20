0f15b667132ec6567080d462e3b01e40618c9310
stlevy <harry1992@gmail.com>
2015-08-20 14:01:32 +0300

    allows breaking of an inheritence rule to many derivation rules.

--- src/main/java/org/spartan/fajita/api/bnf/rules/InheritenceRule.java
+++ src/main/java/org/spartan/fajita/api/bnf/rules/InheritenceRule.java
@@ -1,7 +1,10 @@
 package org.spartan.fajita.api.bnf.rules;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.spartan.fajita.api.bnf.symbols.NonTerminal;
 import org.spartan.fajita.api.bnf.symbols.Symbol;
@@ -26,5 +29,12 @@ public class InheritenceRule extends Rule {
     @Override
     public List<Symbol> getChildren() {
 	return new ArrayList<Symbol>(subtypes);
+    }
+
+    public Set<DerivationRule> getAsDerivationRules() {
+	HashSet<DerivationRule> $ = new HashSet<>();
+	for (NonTerminal nonTerminal : subtypes)
+	    $.add(new DerivationRule(lhs, Arrays.asList(nonTerminal), 0));
+	return $;
     }
 }
