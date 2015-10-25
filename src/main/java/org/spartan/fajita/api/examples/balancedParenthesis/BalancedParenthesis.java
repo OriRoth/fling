486fb50008ce16fec7be9187cfbe2018ba4b374e
stlevy <harry1992@gmail.com>
2015-10-25 09:36:43 +0200

    chagnes balanced parethesis implementation

--- src/main/java/org/spartan/fajita/api/examples/balancedParenthesis/BalancedParenthesis.java
+++ src/main/java/org/spartan/fajita/api/examples/balancedParenthesis/BalancedParenthesis.java
@@ -1,48 +1,20 @@
 package org.spartan.fajita.api.examples.balancedParenthesis;
 
-import static org.spartan.fajita.api.examples.balancedParenthesis.BalancedParenthesis.NT.BALANCED;
-import static org.spartan.fajita.api.examples.balancedParenthesis.BalancedParenthesis.Term.lp;
-import static org.spartan.fajita.api.examples.balancedParenthesis.BalancedParenthesis.Term.rp;
+import static org.spartan.fajita.api.examples.balancedParenthesis.BalancedParenthesis.NT.*;
+import static org.spartan.fajita.api.examples.balancedParenthesis.BalancedParenthesis.Term.*;
 
 import org.spartan.fajita.api.bnf.BNF;
 import org.spartan.fajita.api.bnf.BNFBuilder;
 import org.spartan.fajita.api.bnf.symbols.NonTerminal;
 import org.spartan.fajita.api.bnf.symbols.Terminal;
 import org.spartan.fajita.api.bnf.symbols.Type;
-import org.spartan.fajita.api.examples.balancedParenthesis.states.Q0;
-import org.spartan.fajita.api.examples.balancedParenthesis.states.Q1;
-import org.spartan.fajita.api.examples.balancedParenthesis.states.Q2;
-import org.spartan.fajita.api.examples.balancedParenthesis.states.Q4;
 import org.spartan.fajita.api.generators.BaseStateSpec;
 import org.spartan.fajita.api.generators.typeArguments.TypeArgumentManager;
 import org.spartan.fajita.api.parser.LRParser;
 
 public class BalancedParenthesis {
-  @SuppressWarnings("unused") public static void expressionBuilder() {
-    Q0 q0 = new Q0();
-    Q2<Q0, Q1> lp1 = q0.lp();
-    Q2<Q2<Q0, ?>, Q4> lp2 = lp1.lp();
-    Q2<Q2<Q2<Q0, ?>, ?>, Q4> lp3 = lp2.lp();
-    Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, Q4> lp4 = lp3.lp();
-    Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, Q4> lp5 = lp4.lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, Q4> lp6 = lp5.lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp7 = lp6.lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp8 = lp7.lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp9 = lp8.lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp10 = lp9.lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp11 = lp10.lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp12 = lp11.lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp13 = lp12.lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp14 = lp13.lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp15 = lp14.lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp16 = lp15
-        .lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp17 = lp16
-        .lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp18 = lp17
-        .lp();
-    Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q2<Q0, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, ?>, Q4> lp19 = lp18
-        .lp();
+  public static void expressionBuilder() {
+    //
   }
 
   static enum Term implements Terminal {
---
+++ src/main/java/org/spartan/fajita/api/examples/balancedParenthesis/States.java
@@ -0,0 +1,170 @@
+package org.spartan.fajita.api.examples.balancedParenthesis;
+
+import org.spartan.fajita.api.parser.stack.EmptyStack;
+import org.spartan.fajita.api.parser.stack.IStack;
+
+class States {
+  @java.lang.SuppressWarnings({
+      "rawtypes" }) public abstract static class BaseState<Stack extends IStack<?>, lp extends BaseState, build extends BaseState, rp extends BaseState, BALANCED extends BaseState>
+          implements IStack<Stack> {
+    protected final Stack stack;
+
+    BaseState(final Stack stack) {
+      this.stack = stack;
+    }
+    protected lp lp() {
+      throw new ParseError("unexpected symbol on state " + getClass().getSimpleName());
+    }
+    protected build build() {
+      throw new ParseError("unexpected symbol on state " + getClass().getSimpleName());
+    }
+    protected rp rp() {
+      throw new ParseError("unexpected symbol on state " + getClass().getSimpleName());
+    }
+    protected BALANCED BALANCED() {
+      throw new ParseError("unexpected symbol on state " + getClass().getSimpleName());
+    }
+  }
+
+  @java.lang.SuppressWarnings({ "serial" }) public static class ParseError extends java.lang.RuntimeException {
+    ParseError(final java.lang.String msg) {
+      super(msg);
+    }
+  }
+
+  public static class ErrorState extends BaseState<EmptyStack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    ErrorState() {
+      super(new EmptyStack());
+    }
+  }
+
+  static class Q0 extends BaseState<EmptyStack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q0() {
+      super(new EmptyStack());
+    }
+  }
+
+  static class Q1<Stack extends BaseState<?, ?, ?, ?, ?>> extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q1(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q1ʹ<Stack extends BaseState<?, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q1ʹ(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q2<Stack extends BaseState<?, ?, ?, ?, ?>> extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q2(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q2ʹ<Stack extends BaseState<?, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q2ʹ(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q3<Stack extends BaseState<? extends BaseState<?, ?, ?, ?, ?>, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q3(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q3ʹ<Stack extends BaseState<? extends BaseState<?, ?, ?, ?, ?>, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q3ʹ(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q4<Stack extends BaseState<? extends BaseState<?, ?, ?, ?, ?>, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q4(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q4ʹ<Stack extends BaseState<? extends BaseState<?, ?, ?, ?, ?>, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q4ʹ(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q5<Stack extends BaseState<?, ?, ?, ?, ?>, BALANCED_1_rp extends BaseState<?, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q5(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q5ʹ<Stack extends BaseState<?, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q5ʹ(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q6<Stack extends BaseState<? extends BaseState<?, ?, ?, ?, ?>, ?, ?, ?, ?>, BALANCED_2_rp extends BaseState<?, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q6(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q6ʹ<Stack extends BaseState<? extends BaseState<?, ?, ?, ?, ?>, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q6ʹ(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q7<Stack extends BaseState<? extends BaseState<?, ?, ?, ?, ?>, ?, ?, ?, ?>, BALANCED_2_rp extends BaseState<?, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q7(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q7ʹ<Stack extends BaseState<? extends BaseState<?, ?, ?, ?, ?>, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q7ʹ(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q8<Stack extends BaseState<? extends BaseState<? extends BaseState<?, ?, ?, ?, ?>, ?, ?, ?, ?>, ?, ?, ?, ?>, BALANCED_3_rp extends BaseState<?, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q8(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q8ʹ<Stack extends BaseState<? extends BaseState<? extends BaseState<?, ?, ?, ?, ?>, ?, ?, ?, ?>, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q8ʹ(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q9<Stack extends BaseState<? extends BaseState<? extends BaseState<?, ?, ?, ?, ?>, ?, ?, ?, ?>, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q9(final Stack stack) {
+      super(stack);
+    }
+  }
+
+  static class Q9ʹ<Stack extends BaseState<? extends BaseState<? extends BaseState<?, ?, ?, ?, ?>, ?, ?, ?, ?>, ?, ?, ?, ?>>
+      extends BaseState<Stack, ErrorState, ErrorState, ErrorState, ErrorState> {
+    Q9ʹ(final Stack stack) {
+      super(stack);
+    }
+  }
+}
