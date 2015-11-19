f7acac27846b1243d3e8a0352f115aa565974151
Yossi Gil <yossi.gil@gmail.com>
2015-11-19 14:44:16 +0200

    Split the files

--- src/main/java/sandbox/Delme.java
+++ src/main/java/sandbox/Delme.java
@@ -1,7 +1,5 @@
 package sandbox;
 
-import sandbox.Stack.Bottom;
-import sandbox.Stack.Push;
 import sandbox.Γ.γ1;
 import sandbox.Γ.γ2;
 
@@ -13,21 +11,27 @@ class Γ {
   }
 }
 
-abstract class Stack<Self extends Stack>{
+abstract class Stack<Self extends Stack> {
+  Push<γ1, Self> γ1() {
+    return push((γ1) null);
+  }
+  Push<γ2, Self> γ2() {
+    return push((γ2) null);
+  }
   Γ top() {
     return null;
   }
   Stack pop() {
     return null;
   }
-  <γ extends Γ> Push<γ, Self> push(γ g) {
+  private <γ extends Γ> Push<γ, Self> push(γ g) {
     return null;
   }
 
   static class Bottom extends Stack<Bottom> {
   }
 
-  static class Push<Head extends Γ, Tail extends Stack> extends Stack<Push<Head,Tail>> {
+  static class Push<Head extends Γ, Tail extends Stack> extends Stack<Push<Head, Tail>> {
     Head head;
     Tail tail;
 
@@ -41,60 +45,12 @@ abstract class Stack<Self extends Stack>{
     Tail pop() {
       return tail;
     }
-    
   }
-}
-
-class Delme {
   public static void main(String[] args) {
     Push<γ1, Push<γ2, Push<γ1, Push<γ2, Stack.Bottom>>>> t;
-    Push<γ1, Bottom> push = new Stack.Bottom().push((γ1)null);
-    Push<γ2, Push<γ1, Bottom>> push2 = push.push((γ2)null);
-    
+    Push<γ1, Bottom> push = new Stack.Bottom().push((γ1) null);
+    Push<γ2, Push<γ1, Bottom>> push2 = push.push((γ2) null);
+    Push<γ1, Push<γ1, Push<γ2, Push<γ1, Push<γ1, Bottom>>>>> a = ((Stack.Bottom) null).γ1().γ1().γ2().γ1().γ1();
   }
 }
 
-abstract class Qʹ {
-  static final class ¤ extends Qʹ {
-  }
-
-  static abstract class Q<CurrentStack extends Stack, Top1 extends Γ, Top2 extends Γ> extends Qʹ {
-    abstract Qʹ σ1();
-    abstract Qʹ σ2();
-
-    static abstract class q1<CurrentStack extends Stack, Top1 extends Γ, Top2 extends Γ> extends Q<CurrentStack, Top1, Top2> {
-    }
-
-    static final class q1_1_1<CurrentStack extends Stack> extends q1<CurrentStack, γ1, γ1> {
-      Q σ1() {
-        return null;
-      }
-      ¤ σ2() {
-        return null;
-      }
-    }
-
-    static class q1_1_2<CurrentStack extends Stack> extends q1<CurrentStack, γ1, γ2> {
-    }
-
-    static class q1_2_1<CurrentStack extends Stack> extends q1<CurrentStack, γ2, γ1> {
-    }
-
-    static class q1_2_2<CurrentStack extends Stack> extends q1<CurrentStack, γ2, γ2> {
-    }
-
-    static abstract class q2<CurrentStack extends Stack, Top1 extends Γ, Top2 extends Γ> extends Q<CurrentStack, Top1, Top2> {
-    }
-
-    static class q2_1_1<CurrentStack extends Stack> extends q2<CurrentStack, γ1, γ1> {
-    }
-
-    static class q2_1_2<CurrentStack extends Stack> extends q2<CurrentStack, γ1, γ2> {
-    }
-
-    static class q2_2_1<CurrentStack extends Stack> extends q2<CurrentStack, γ2, γ1> {
-    }
-
-    static class q2_2_2<CurrentStack extends Stack> extends q2<CurrentStack, γ2, γ2> {
-    }
-  }
