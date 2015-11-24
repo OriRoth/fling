fb01dc01c474d7bddc549360a75204f113cefd77
stlevy <harry1992@gmail.com>
2015-11-24 16:53:36 +0200

    escape unicode

--- Manuscripts/ECOOP16/escape.py
+++ Manuscripts/ECOOP16/escape.py
@@ -1,8 +1,16 @@
 import sys
-inp = raw_input().decode('UTF-8')
-for c in inp:
-  try:
-    c.decode('ASCII')
-    sys.stdout.write("%c" %c)
-  except UnicodeError:
-    sys.stdout.write(u'\xa2$%c$\xa2' %c)
+out = u''
+while True:
+  try: 
+    raw = raw_input()  
+  except EOFError:
+    break
+  inp = raw.decode('UTF-8')
+  for c in inp:
+    try:
+      c.decode('ASCII')
+      out = out + c.decode('UTF-8')
+    except UnicodeError:
+      out = out + u'\xa2$'+c+u'$\xa2'
+  out = out +'\n'
+print out[:-1]
