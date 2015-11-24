0eea5bb4a0a5e2f68b2829ba9928e4213194f2bd
Yossi Gil <yossi.gil@gmail.com>
2015-11-24 13:39:37 +0200

    Sync

--- Manuscripts/ECOOP16/escape.py
+++ Manuscripts/ECOOP16/escape.py
@@ -1,8 +1,21 @@
+#!/usr/bin/python
 import sys
-inp = raw_input().decode('UTF-8')
-for c in inp:
-  try:
-    c.decode('ASCII')
-    sys.stdout.write("%c" %c)
-  except UnicodeError:
-    sys.stdout.write(u'\xa2$%c$\xa2' %c)
+while True:
+    escape = u'\xa2'
+    try: 
+        raw = raw_input()
+    except EOFError:
+        break
+    if not raw: break
+    inp = raw.decode('UTF-8')
+    for c in inp:
+      try:
+        c.decode('ASCII')
+        sys.stdout.write("%c" %c)
+      except UnicodeError:
+        sys.stdout.write('%c' %escape)
+        sys.stdout.write('$')
+        sys.stdout.write('%c' %c) 
+        sys.stdout.write('$')
+        sys.stdout.write('%c' %escape)
+    sys.stdout.write("\n")
