af3fc04569a6bac8073985ceebfd88c61c113d96
Yossi Gil <yossi.gil@gmail.com>
2015-11-27 14:31:28 +0200

    Finish up the stack

--- src/main/java/automaton/Domain.java
+++ src/main/java/automaton/Domain.java
@@ -43,27 +43,27 @@ public class Domain {
   //stack.listing
   
   public static abstract class Stack<Rest extends Stack<?>> { 
-    public abstract Rest pop(); 
     public abstract Γʹ top();
+    public abstract Rest pop(); 
     public abstract Stack<?> γ1(); 
     public abstract Stack<?> γ2();
     public static final E empty = null; 
-    public static final class E extends Stack<¤> {
-      @Override public ¤ pop() { return null; } 
+    public static final class E extends Stack<¤> { // Type of an empty stack
       @Override public Γʹ.¤ top() { return null; }
+      @Override public ¤ pop() { return null; } 
       @Override public P<γ1, E> γ1() { return null; } 
       @Override public P<γ2, E> γ2() { return null; }
     }
     private static final class ¤ extends Stack<¤> {
-      @Override public ¤ pop() { return null; } 
       @Override public Γʹ.¤ top() { return null; }
+      @Override public ¤ pop() { return null; } 
       @Override public ¤ γ1() { return null; } 
       @Override public ¤ γ2() { return null; }
     }
-    // Type constructor making it possible to push an unlimited number of items (types) into the stack: 
+    // Recursive generic type making it possible to generate stacks of unounded depth: 
     public static class P<Top extends Γ, Rest extends Stack<?>> extends Stack<Rest> {
-      @Override public Rest pop() { return null; }
       @Override public Top top() { return null; } 
+      @Override public Rest pop() { return null; }
       @Override public P<γ1, P<Top,Rest>> γ1() { return null; } 
       @Override public P<γ2, P<Top,Rest>> γ2() { return null; }
     }
