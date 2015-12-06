1da3a0d4e90898559665a878862a02e49bb6c552
stlevy <harry1992@gmail.com>
2015-12-06 12:56:32 +0200

    fixes script

--- Manuscripts/Figures/timePlot.py
+++ Manuscripts/Figures/timePlot.py
@@ -9,9 +9,7 @@ code = 'package automaton; \n\
   } \n\
   \n\
   public static void main(String[] args) { \n\
-  Cons<?,?> s =new Cons<String,String>(){ \n\
-	public Cons<Cons<String,String>,Cons<String,String>>d() {return null;}\n\
-}%s; \n\
+  ((Cons<?,?>)null)%s; \n\
    \n\
   } \n\
 }'
@@ -19,13 +17,20 @@ code = 'package automaton; \n\
 def n_invocations(n): 
   return code %('.d()'*n,)
 
+if len(sys.argv)==1:
+  raise Exception('argv is empty. First parameter must be the maximal sequence length')
 runtimes = []
-for i in xrange(0,int(sys.argv[1])):
+output = 'kill.csv'
+
+out = open(output,'w')
+out.write('x,y')
+for i in xrange(1,1+int(sys.argv[1])):
   f = open('$.java','w')
   f.write(n_invocations(i))
   f.flush()
   f.close()
   current = [timeit.timeit(r'os.system("javac $.java")','import os',number=1)]
-  print '%i,%f,' %(i,current[0])
+  out.write(',\n%i,%f' %(i,current[0]))
   runtimes += current
+print 'output printed to file %s' %(output,)
 print runtimes
