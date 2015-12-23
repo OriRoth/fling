e4a5608f939f4b0f32be65f5c5ad7de5c20605b6
stlevy <harry1992@gmail.com>
2015-12-23 16:22:27 +0200

    changes example of proof

--- src/main/java/automaton/jDPDA.java
+++ src/main/java/automaton/jDPDA.java
@@ -2,6 +2,7 @@ package automaton;
 import static automaton.A.build;
 
 import automaton.A.C.Cγ1;
+import automaton.A.C.Cγ2;
 import automaton.A.C.E;
 import automaton.A.C.¤;
 //@formatter:off
@@ -18,31 +19,29 @@ class A { // Encode automaton ¢$A$¢
   // Configuration of the automaton
   /// begin{configuration}
   interface C<      // Generic parameters:
-    Rest extends C, // The rest of the stack, for pop operations
-    Jγ1 extends C,  // Type of ¢$~\textsf{jump}(\gamma1)$¢, may be rest, or anything in it. 
-    Jγ2 extends C,  // Type of ¢$~\textsf{jump}(\gamma2)$¢, may be rest, or anything in it. 
+    Rest extends C, // The rest of the stack, for pop operations 
     JRγ1 extends C, // Type of ¢$~\cc{Rest}.\textsf{jump}(\gamma1)$¢, may be rest, or anything in it. 
     JRγ2 extends C  // Type of ¢$~\cc{Rest}.\textsf{jump}(\gamma2)$¢, may be rest, or anything in it.  
   >
   {
     ΣΣ $();        // δ transition on end of input; invalid language by default 
-    C σ1();         // δ transition on σ¢1¢; dead end by default
-    C σ2();         // δ transition on σ¢2¢; dead end by default
-    public interface E extends C<¤,¤,¤,¤,¤> { /* Empty stack configuration */ }
-    interface ¤ extends C<¤,¤,¤,¤,¤> { /* Error configuration. */ }
+    C a();         // δ transition on ¢a¢; dead end by default
+    C b();         // δ transition on ¢b¢; dead end by default
+    C c();         // δ transition on ¢c¢; dead end by default
+    public interface E extends C<¤,¤,¤> { /* Empty stack configuration */ }
+    interface ¤ extends C<¤,¤,¤> { /* Error configuration. */ }
   // end{configuration}
   //begin{many}
      interface Cγ1< // Configuration when ¢$\gamma1$¢ is at top
        Rest extends C, JRγ1 extends C, JRγ2 extends C
      > extends 
-       // ¢$J\gamma1$¢ must be ¢$\textsf{Rest}$¢.
-       C<Rest, Rest, JRγ2, JRγ1, JRγ2>   
+       C<Rest, JRγ1, JRγ2>   
      // end{many} 
-       ,γ1σ1_Push_γ1γ1<Rest,JRγ1,JRγ2>
-       ,γ1σ2_Push_γ2γ2<Rest,JRγ1,JRγ2>
+       ,γ1a_Push_γ1γ1γ2<Rest,JRγ1,JRγ2>
      // begin{many}
      {
      // end{many}
+       @Override L $();
      // begin{many}
      }
      // end{many}
@@ -50,22 +49,24 @@ class A { // Encode automaton ¢$A$¢
      interface Cγ2< // Configuration when ¢$\gamma2$¢ is at top
        Rest extends C, JRγ1 extends C, JRγ2 extends C
      > extends 
-       // ¢$J\gamma2$¢ must be ¢$\textsf{Rest}$¢. 
-       C<Rest,  JRγ1, Rest, JRγ1, JRγ2>  
+       C<Rest, JRγ1, JRγ2>
+     // end{many}
+       ,γ2a_Push_γ2γ2<Rest,JRγ1,JRγ2>
+     // begin{many}
     { 
      // end{many}
-      @Override L $();
-      @Override JRγ1 σ2();
+       @Override Rest b();
+       @Override JRγ1 c();
   //begin{many}
     }
   // end{many}
-    interface γ1σ1_Push_γ1γ1<Rest extends C,JRγ1 extends C,JRγ2 extends C>{
-      // Sidekick of ¢$\delta(\gamma_1,\sigma_1)=\textsf{push}(\gamma_1,\gamma_1)$¢
-      Cγ1<Cγ1<Rest, JRγ1, JRγ2 >, Rest, JRγ2> σ1();
+    interface γ1a_Push_γ1γ1γ2<Rest extends C,JRγ1 extends C,JRγ2 extends C>{
+      // Sidekick of ¢$\delta(\gamma_1,a)=\textsf{push}(\gamma_1,\gamma_1,\gamma_2)$¢
+      Cγ2<Cγ1<Cγ1<Rest, JRγ1, JRγ2 >, Rest, JRγ2>,Cγ1<Rest, JRγ1, JRγ2 >,JRγ2> a();
     }
-    interface γ1σ2_Push_γ2γ2<Rest extends C,JRγ1 extends C,JRγ2 extends C>{
-      // Sidekick of ¢$\delta(\gamma_1,\sigma_2)=\textsf{push}(\gamma_2,\gamma_2)$¢
-      Cγ2<Cγ2<Rest, JRγ1, JRγ2>, JRγ1, Rest> σ2();
+    interface γ2a_Push_γ2γ2<Rest extends C,JRγ1 extends C,JRγ2 extends C>{
+      // Sidekick of ¢$\delta(\gamma_2,a)=\textsf{push}(\gamma_2,\gamma_2)$¢
+      Cγ2<Cγ2<Rest, JRγ1, JRγ2>, JRγ1, Rest> a();
     }
   }
   //begin{many}
@@ -75,20 +76,20 @@ class A { // Encode automaton ¢$A$¢
   //begin{cases}
   static void isL( L l) {/**/}
   static void accepts() {
-    isL(A.build.σ2().$());
-    isL(A.build.σ1().σ2().$()); 
-    isL(A.build.σ1().σ1().σ2().σ2().σ1().σ2().$()); 
+    isL(A.build.$());
+    isL(A.build.a().c().$());
+    isL(A.build.a().b().$()); 
+    isL(A.build.a().a().b().c().a().b().$()); 
   }
   static void rejects() {
-    isL(A.build.$()); 
-    isL(A.build.σ1().$()); 
-    isL(A.build.σ2().σ1().$());
-    isL(A.build.σ2().σ2().$());
-    isL(A.build.σ1().σ2().σ1().$());  
-    isL(A.build.σ1().σ2().σ2().σ1().$());  
+    isL(A.build.a().$()); 
+    isL(A.build.b().a().$());
+    isL(A.build.b().b().$());
+    isL(A.build.a().b().a().$());  
+    isL(A.build.a().b().b().a().$());  
     //end{cases}
-    isL(A.build.σ1().σ2().σ2().σ2().σ1().$()); 
-    isL(A.build.σ1().σ2().σ2().σ2().σ2().σ2().σ2().σ2().σ2().σ1().$());  
+    isL(A.build.a().b().b().b().a().$()); 
+    isL(A.build.a().b().b().b().b().b().b().b().b().a().$());  
     //begin{cases}
   }
   //end{cases}
