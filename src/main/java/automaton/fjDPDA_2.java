40df259fde0554e04a16133240df7fd7960b118f
stlevy <harry1992@gmail.com>
2015-12-29 18:00:32 +0200

    adding commentary

--- src/main/java/automaton/fjDPDA_2.java
+++ src/main/java/automaton/fjDPDA_2.java
@@ -7,8 +7,14 @@ import automaton.fjDPDA_2.A.q0lp_Push_q0_f_q2.q2_recursive_q0;
 import automaton.fjDPDA_2.A.q2lp_Push_q2_f_q4.q4_recursive_q2;
 /**
  * JLR parser for the grammar : S -> B B -> B lp B rp | ε
+ * the grammar generates the language of balanced parenthesis
+ *
+ * δ(q₀,lp) = q₀ { lp→q₂ } q₂           |
+ * δ(q₂,lp) = q₂ { lp→q₄, rp→q₅ } q4    | δ(q₂,rp) = q₂ q₅
+ * δ(q₄,lp) = q₄ { lp→q₄, rp→q₇ } q4    | δ(q₄,rp) = q₄ q₇
+ * δ(q₅,$)  = jump($)                   | δ(q₅,lp) = jump(lp)
+ * δ(q₇,lp) = jump(lp)                  | δ(q₇,rp) = jump(rp)
  * 
- * that generates the language : L = a^+(b|c)
  */
 @SuppressWarnings({ "unused" }) class fjDPDA_2 {
   static class A {
@@ -18,21 +24,21 @@ import automaton.fjDPDA_2.A.q2lp_Push_q2_f_q4.q4_recursive_q2;
     interface ¤ { /* Error configuration. */ }
     //@formatter:on
 
-    interface Cq0<Rest, JR_lp, JR_rp> extends 
+    interface Cq0<Rest, JR_lp, JR_rp> extends
       q0lp_Push_q0_f_q2<Rest, JR_lp, JR_rp, Cq0<Rest, JR_lp, JR_rp>> {
       // inherited lp()
       L $();
     }
 
-    interface Cq2<Rest, JR_lp, JR_rp> extends 
+    interface Cq2<Rest, JR_lp, JR_rp> extends
       q2lp_Push_q2_f_q4<Rest, JR_lp, JR_rp, Cq2<Rest, JR_lp, JR_rp>>,
       q2rp_Push_q2_q5<Rest, JR_lp, JR_rp, Cq2<Rest, JR_lp, JR_rp>> {
       // inherited lp()
       // inherited rp()
     }
 
-    interface Cq4<Rest, JR_lp, JR_rp> extends 
-      q4lp_Push_q4_f_q4<Rest, JR_lp, JR_rp, Cq4<Rest, JR_lp, JR_rp>> , 
+    interface Cq4<Rest, JR_lp, JR_rp> extends
+      q4lp_Push_q4_f_q4<Rest, JR_lp, JR_rp, Cq4<Rest, JR_lp, JR_rp>> ,
       q4rp_Push_q4_q7<Rest, JR_lp, JR_rp, Cq4<Rest, JR_lp, JR_rp>>{
       // inherited lp();
       // inherited rp();
@@ -48,7 +54,7 @@ import automaton.fjDPDA_2.A.q2lp_Push_q2_f_q4.q4_recursive_q2;
       JR_rp rp();
     }
 
-    //@formatter:off
+    // Side-kicks for pushes
     interface q0lp_Push_q0_f_q2<Rest,JR_lp,JR_rp,Me>{
       interface q2_recursive_q0<Rest,JR_rp> extends Cq2<Rest,q2_recursive_q0<Rest,JR_rp>,JR_rp>{
         // auxiliary
@@ -67,7 +73,7 @@ import automaton.fjDPDA_2.A.q2lp_Push_q2_f_q4.q4_recursive_q2;
     interface q4rp_Push_q4_q7<Rest,JR_lp,JR_rp,Me>{
       Cq7<Me,JR_lp,JR_rp> rp();
     }
-    interface q4lp_Push_q4_f_q4<Rest,JR_lp,JR_rp,Me>{ 
+    interface q4lp_Push_q4_f_q4<Rest,JR_lp,JR_rp,Me>{
       interface q4_recursive_q4<Rest,JR_lp,JR_rp> extends Cq4<Rest,q4_recursive_q4<Rest,JR_lp,JR_rp>,Cq7<Rest,JR_lp,JR_rp>>{
         //auxiliary
       }
@@ -79,18 +85,18 @@ import automaton.fjDPDA_2.A.q2lp_Push_q2_f_q4.q4_recursive_q2;
 
     static void accepts() {
       A.build.$();
-      
-      //        (    )
+
+      // ( )
       A.build.lp().rp().$();
-      
-      //        (    (    (    (    )    (    )    )    )    )
+
+      // (((() ())))
       A.build.lp().lp().lp().lp().rp().lp().rp().rp().rp().rp().$();
     }
     static void rejects() {
       A.build.rp();
       A.build.lp().$();
       A.build.lp().rp().rp();
-      
+
     }
   }
 }
