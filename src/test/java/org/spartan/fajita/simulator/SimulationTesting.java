33f9b5a5ca4c920e331f9f78ea2b1ce4139ab46f
stlevy <harry1992@gmail.com>
2016-02-18 21:38:40 +0200

    test improve.

--- src/test/java/org/spartan/fajita/simulator/SimulationTesting.java
+++ src/test/java/org/spartan/fajita/simulator/SimulationTesting.java
@@ -1,6 +1,6 @@
 package org.spartan.fajita.simulator;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.spartan.fajita.simulator.SimulationTesting.NT.A;
 import static org.spartan.fajita.simulator.SimulationTesting.NT.B;
 import static org.spartan.fajita.simulator.SimulationTesting.NT.C;
@@ -47,64 +47,43 @@ import org.spartan.fajita.api.jlr.simulator.JLRSimulator;
   @BeforeClass public static void ab_list() {
     System.out.println("Testing regular language: a*b*");
     BNF bnf = new BNFBuilder(Term.class, NT.class) //
-        .start(S) //
+        .start(A) //
         .derive(S).to(A).and(B) //
-        .derive(B).to(b) // Left recursive
+        .derive(B).to(B).and(b).orNone() // Left recursive
         .derive(A).to(a).and(A).orNone() // Right recursive
         // .derive(C).to(c) //
         .finish();
     ab_list_jlr = new JLRRecognizer(bnf);
     Main.lrAutomatonVisualisation(bnf);
   }
-  @Test public void testtt(){
-    test_ab("", false);
-    test_ab("a", false);
-    test_ab("b", true);
-    test_ab("ab", true);
-    test_ab("ba", false);
-    test_ab("abbbbbbbbb", false);
-    test_ab("aaaaaaaab", true);
+  @Test public void epsilon() {
+    test_ab( "", true);
   }
-//  @BeforeClass public static void ab_list() {
-//    System.out.println("Testing regular language: a*b*");
-//    BNF bnf = new BNFBuilder(Term.class, NT.class) //
-//        .start(A) //
-//        .derive(S).to(A).and(B) //
-//        .derive(B).to(B).and(b).orNone() // Left recursive
-//        .derive(A).to(a).and(A).orNone() // Right recursive
-//        // .derive(C).to(c) //
-//        .finish();
-//    ab_list_jlr = new JLRRecognizer(bnf);
-//    Main.lrAutomatonVisualisation(bnf);
-//  }
-//  @Test public void epsilon() {
-//    test(ab_list_jlr, "", true);
-//  }
-//  @Test public void single_a() {
-//    test(ab_list_jlr, "a", true);
-//  }
-//  @Test public void single_b() {
-//    test(ab_list_jlr, "b", true);
-//  }
-//  @Test public void ab() {
-//    test(ab_list_jlr, "ab", true);
-//  }
-//  @Test public void illegal_ba() {
-//    test(ab_list_jlr, "ba", false);
-//  }
-//  @Test public void b_sequence() {
-//    test(ab_list_jlr, "bbbbbbbbbbbbbbbb", true);
-//  }
-//  @Test public void a_sequence() {
-//    test(ab_list_jlr, "aaaaaaaaa", true);
-//  }
-//  @Test public void long_ab() {
-//    test(ab_list_jlr, "aaaaaaaabbbbbbbbbbbbbbbb", true);
-//  }
+  @Test public void single_a() {
+    test_ab( "a", true);
+  }
+  @Test public void single_b() {
+    test_ab( "b", true);
+  }
+  @Test public void ab() {
+    test_ab( "ab", true);
+  }
+  @Test public void illegal_ba() {
+    test_ab( "ba", false);
+  }
+  @Test public void b_sequence() {
+    test_ab( "bbbbbbbbbbbbbbbb", true);
+  }
+  @Test public void a_sequence() {
+    test_ab( "aaaaaaaaa", true);
+  }
+  @Test public void long_ab() {
+    test_ab( "aaaaaaaabbbbbbbbbbbbbbbb", true);
+  }
   public static void test_ab(String input, boolean expected) {
-    test(ab_list_jlr,input,expected);
+    test(ab_list_jlr, input, expected);
   }
   public static void test(JLRRecognizer jlr, String input, boolean expected) {
-    assertTrue(JLRSimulator.runJLR(jlr, input) == expected);
+    assertEquals(JLRSimulator.runJLR(jlr, input) ,  expected);
   }
 }
