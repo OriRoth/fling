7ec84c2411b4f2d16f7977345bba0357bbe9de81
stlevy <harry1992@gmail.com>
2016-03-22 19:40:07 +0200

    some fixes and improvments

---
+++ src/main/java/automaton/oopsla/FlufluTest.java
@@ -0,0 +1,13 @@
+package automaton.oopsla;
+
+import com.javax0.fluflu.Fluentize;
+import com.javax0.fluflu.Transition;
+public class FlufluTest {
+  // begin{full}
+  @Fluentize(className = "L", startState = "Q0", startMethod = "a")
+  public abstract class ToBeFluentized {
+    @Transition(from = "Q0", to = "Q0") public void a() {
+    }
+  }
+  // end{full}
+}
