4351bbf73043b8dada9dd78d4921e59fa6a5ee13
stlevy <harry1992@gmail.com>
2016-03-22 19:47:47 +0200

    more work on introduction

--- src/main/java/automaton/oopsla/FlufluTest.java
+++ src/main/java/automaton/oopsla/FlufluTest.java
@@ -4,7 +4,8 @@ import com.javax0.fluflu.Fluentize;
 import com.javax0.fluflu.Transition;
 public class FlufluTest {
   // begin{full}
-  @Fluentize(className = "L", startState = "Q0", startMethod = "a")
+  @Fluentize(className = "L", startState = "Q0"
+      , startMethod = "a")
   public abstract class ToBeFluentized {
     @Transition(from = "Q0", to = "Q0") public void a() {
     }
