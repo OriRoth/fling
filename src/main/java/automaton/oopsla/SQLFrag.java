f24e21f3931fd6ce90f72926f57caa4b391b6d7f
stlevy <harry1992@gmail.com>
2016-03-23 12:34:23 +0200

    work on introduction

---
+++ src/main/java/automaton/oopsla/SQLFrag.java
@@ -0,0 +1,59 @@
+package automaton.oopsla;
+
+import static automaton.oopsla.SQLFrag.SQLNonterminals.Operator;
+import static automaton.oopsla.SQLFrag.SQLNonterminals.Quant;
+import static automaton.oopsla.SQLFrag.SQLNonterminals.Query;
+import static automaton.oopsla.SQLFrag.SQLNonterminals.Where;
+import static automaton.oopsla.SQLFrag.SQLTerminals.all;
+import static automaton.oopsla.SQLFrag.SQLTerminals.column;
+import static automaton.oopsla.SQLFrag.SQLTerminals.columns;
+import static automaton.oopsla.SQLFrag.SQLTerminals.equals;
+import static automaton.oopsla.SQLFrag.SQLTerminals.from;
+import static automaton.oopsla.SQLFrag.SQLTerminals.greaterThan;
+import static automaton.oopsla.SQLFrag.SQLTerminals.lowerThan;
+import static automaton.oopsla.SQLFrag.SQLTerminals.select;
+import static automaton.oopsla.SQLFrag.SQLTerminals.where;
+
+import org.spartan.fajita.api.bnf.BNFBuilder;
+import org.spartan.fajita.api.bnf.symbols.NonTerminal;
+import org.spartan.fajita.api.bnf.symbols.Terminal;
+
+public class SQLFrag {
+  
+  class Table{} 
+  class Column {}
+  class Expr {}
+  
+  
+  static 
+    // begin{terminals}
+  enum SQLTerminals implements Terminal{
+    select,from,all,columns,
+    where,column,equals,greaterThan,lowerThan;
+  }
+    // end{terminals}
+  static
+    // begin{nonterminals}
+  enum SQLNonterminals implements NonTerminal{
+    Query,Quant,Where,Operator;
+  }
+    // end{nonterminals}
+
+  void defineBNF(){
+    // begin{BNF}
+    new BNFBuilder(SQLTerminals.class, SQLNonterminals.class)
+      .start(Query)
+      .derive(Query).to(select)
+                    .and(Quant).and(from,Table.class).and(Where)
+      .derive(Quant).to(all)
+                    .or(columns,Column[].class)
+      .derive(Where).to(where)
+                    .and(column,Column.class).and(Operator)
+                    .orNone()
+      .derive(Operator).to(equals,Expr.class)
+                       .or(greaterThan,Expr.class)
+                       .or(lowerThan,Expr.class)
+      .finish();
+    // end{BNF}
+  }
+}
