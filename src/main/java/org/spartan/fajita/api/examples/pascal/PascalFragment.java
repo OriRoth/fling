bf31faf2be99c6cbb4bdcb21c2ba120ec7c31941
stlevy <harry1992@gmail.com>
2016-04-04 21:15:47 +0300

    pascal with Fajita

---
+++ src/main/java/org/spartan/fajita/api/examples/pascal/PascalFragment.java
@@ -0,0 +1,54 @@
+package org.spartan.fajita.api.examples.pascal;
+
+import java.io.IOException;
+
+import org.spartan.fajita.api.Main;
+import org.spartan.fajita.api.bnf.BNF;
+import org.spartan.fajita.api.bnf.BNFBuilder;
+import org.spartan.fajita.api.bnf.symbols.NonTerminal;
+import org.spartan.fajita.api.bnf.symbols.Terminal;
+import static org.spartan.fajita.api.examples.pascal.PascalFragment.NT.*;
+import static org.spartan.fajita.api.examples.pascal.PascalFragment.Term.*;
+
+public class PascalFragment {
+  static enum Term implements Terminal {
+    program, begin, end, label, //
+    constant, id, procedure, semi, //
+    pair;
+  }
+
+  static enum NT implements NonTerminal {
+    Program, Parameters, Definitions, //
+    Body, Labels, Constants, Label, //
+    Constant, MoreLabels, MoreConstants, //
+    Nested, Procedure;
+  }
+
+  public static BNF buildBNF() {
+    BNF bnf = new BNFBuilder(Term.class, NT.class) //
+        .start(Program) //
+        .derive(Program).to(program).and(id).and(Parameters).and(semi).and(Definitions).and(Body) //
+        .derive(Body).to(begin).and(end) //
+        .derive(Definitions).to(Labels).and(Constants).and(Nested) //
+        .derive(Labels).to(label).and(Label).and(MoreLabels) //
+        /*           */.orNone() //
+        .derive(Constants).to(constant).and(Constant).and(Constants) //
+        /*           */.orNone() //
+        .derive(Label).to(semi) //
+        .derive(Constant).to(semi)//
+        .derive(MoreLabels).to(Label).and(MoreLabels) //
+        /*           */.orNone() //
+        .derive(MoreConstants).to(Constant).and(MoreConstants) //
+        /*           */.orNone() //
+        .derive(Nested).to(Procedure).and(Nested) //
+        /*           */.orNone() //
+        .derive(Procedure).to(procedure).and(id).and(Parameters).and(semi).and(Definitions).and(Body)//
+        .derive(Parameters).to(pair) //
+        /*           */.orNone() //
+        .go();
+    return bnf;
+  }
+  public static void main(String[] args) throws IOException {
+    Main.apiGenerator(buildBNF());
+  }
+}
