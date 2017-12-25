df9a7b2b69d3d4956f911581a6db4b65ede4911a
Ori Roth <ori.rothh@gmail.com>
2017-12-25 14:36:53 +0200

    sync

---
+++ revision/src/test/java/org/spartan/fajita/revision/motivation/Balanced.java
@@ -0,0 +1,50 @@
+package org.spartan.fajita.revision.motivation;
+
+@SuppressWarnings({ "unchecked", "rawtypes" }) public class Balanced {
+  interface S_<E> {
+    Object $();
+    S_c<c_<S_<E>>, S_<E>> o();
+  }
+
+  interface c_<E> {
+    E c();
+  }
+
+  interface S_c<E, c> {
+    S_c<c_<S_c<E, c>>, S_c<E, c>> o();
+    c c();
+  }
+
+  interface $ {
+    Object $();
+  }
+
+  static class $$$ implements S_, c_, S_c, $ {
+    @Override public $$$ c() {
+      return this;
+    }
+    @Override public $$$ o() {
+      return this;
+    }
+    @Override public $$$ $() {
+      return this;
+    }
+  }
+
+  static S_<$> go() {
+    return new $$$();
+  }
+  public static void main(String[] args) {
+    go() //
+        .o() //
+        .c() //
+        .o() //
+        /**/.o()/**//**/.o() //
+        /**//**/.c() //
+        /**/.c() //
+        /**/.o() //
+        /**/.c() //
+        .c() //
+        .$();
+  }
+}
