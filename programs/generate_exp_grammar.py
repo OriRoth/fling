b08535a37c5ac02d2a70f29e9bc54b789b8d097f
Ori Roth <ori.rothh@gmail.com>
2018-01-09 16:23:05 +0200

    sync

--- programs/generate_exp_grammar.py
+++ programs/generate_exp_grammar.py
@@ -8,19 +8,16 @@ n = 2 ** l
 
 def declare():
   for i in range(2 * n - 1):
-    stdout.write('a' + str(i) + ', ')
-  print ''
-  for i in range(2 * n - 1):
     stdout.write('A' + str(i) + ', ')
   print ''
 
 
 def derive(i, d):
   if d == l:
-    print '.derive(A' + str(i) +  ').to(a' + str(i) + ') //'
+    print '.derive(A' + str(i) +  ').to(a) //'
     return
   i1, i2 = 2 * i + 1, 2 * i + 2
-  print '.derive(A' + str(i) + ').to(a' + str(i) + ', option(A' + str(i1) + '), a' + str(i) + ', option(' + 'A' + str(i2) + '), a' + str(i) + ') //'
+  print '.derive(A' + str(i) + ').to(a, option(A' + str(i1) + '), a, option(' + 'A' + str(i2) + '), a) //'
   derive(i1, d + 1)
   derive(i2, d + 1)
 
