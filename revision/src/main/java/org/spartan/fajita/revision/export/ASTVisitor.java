46b83595a57bc3e2b815fab1532e3f5bd9f04508
Ori Roth <ori.rothh@gmail.com>
2018-01-12 22:28:33 +0200

    sync

--- revision/src/main/java/org/spartan/fajita/revision/export/ASTVisitor.java
+++ revision/src/main/java/org/spartan/fajita/revision/export/ASTVisitor.java
@@ -1,52 +1,102 @@
 package org.spartan.fajita.revision.export;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-@SuppressWarnings({ "unused", "static-method" }) public class ASTVisitor {
+public abstract class ASTVisitor {
   private final Map<Class<?>, Method> preVisit = new HashMap<>();
   private final Map<Class<?>, Method> visit = new HashMap<>();
   private final Map<Class<?>, Method> postVisit = new HashMap<>();
-  private Object currentParent;
 
   // NOTE may be extended to include several AST classes
   public ASTVisitor(Class<? extends AST> astTopClass) {
     for (Class<?> i : astTopClass.getClasses()) {
-      preVisit.put(i, getPreVisitMethod(this, i));
-      visit.put(i, getVisitMethod(this, i));
-      postVisit.put(i, getPostVisitMethod(this, i));
+      Method m = getPreVisitMethod(this, i);
+      if (m != null) {
+        m.setAccessible(true);
+        preVisit.put(i, m);
+      }
+      m = getVisitMethod(this, i);
+      if (m != null) {
+        m.setAccessible(true);
+        visit.put(i, m);
+      }
+      m = getPostVisitMethod(this, i);
+      if (m != null) {
+        m.setAccessible(true);
+        postVisit.put(i, m);
+      }
     }
   }
   public void startVisit(Object node) {
     _visit(node);
   }
-  public boolean preVisit(Object node) {
-    return true;
-  }
-  private void postVisit(Object node) {
-    //
-  }
-  private boolean visit(Object node) {
-    return true;
-  }
-  private final Object parent() {
-    return currentParent;
-  }
   private void _visit(Object o) {
     if (o == null)
       return;
     Class<?> oc = o.getClass();
-//    if (!preVisit(o))
-//      return;
-//    for (Field f : oc.getFields())
-//      _visit(o, f);
+    if (oc.isArray()) {
+      _visitArray(o);
+      return;
+    }
+    if (Either.class.equals(oc)) {
+      _visitEither((Either) o);
+      return;
+    }
+    if (!_doPreVisit(o, oc))
+      return;
+    if (!_doVisit(o, oc))
+      return;
+    _visitChildren(o, oc);
+    _doPostVisit(o, oc);
   }
-  private void _visit(Object o, Field f) {
-    // TODO Auto-generated method stub
+  private void _visitChildren(Object o, Class<?> oc) {
+    for (Field f : oc.getFields())
+      try {
+        _visit(f.get(o));
+      } catch (@SuppressWarnings("unused") IllegalArgumentException | IllegalAccessException e) {
+        assert false : "Should not reach"; // TODO Roth: not true
+      }
+  }
+  private boolean _doPreVisit(Object o, Class<?> oc) {
+    return _do(o, oc, preVisit);
+  }
+  private boolean _doVisit(Object o, Class<?> oc) {
+    return _do(o, oc, visit);
+  }
+  private void _doPostVisit(Object o, Class<?> oc) {
+    _do(o, oc, postVisit);
+  }
+  @SuppressWarnings("boxing") private boolean _do(Object o, Class<?> oc, Map<Class<?>, Method> visitors) {
+    if (!visitors.containsKey(oc))
+      return true;
+    Object $ = null;
+    try {
+      $ = visitors.get(oc).invoke(this, o);
+    } catch (IllegalAccessException | IllegalArgumentException e) {
+      e.printStackTrace();
+      assert false : "Should not happen"; // TODO Roth: not true
+      return false;
+    } catch (InvocationTargetException e) {
+      throw new RuntimeException(e.getTargetException());
+    }
+    if ($ instanceof Boolean)
+      return ((Boolean) $).booleanValue();
+    if (boolean.class.isInstance($))
+      return (boolean) $;
+    return true;
+  }
+  private void _visitArray(Object o) {
+    int length = Array.getLength(o);
+    for (int i = 0; i < length; ++i)
+      _visit(Array.get(o, i));
+  }
+  private void _visitEither(Either e) {
+    _visit(e.get(e.type()));
   }
   private static Method getPreVisitMethod(ASTVisitor v, Class<?> i) {
     return getMethod(v, i, "preVisit");
@@ -59,15 +109,9 @@ import java.util.Map;
   }
   private static Method getMethod(ASTVisitor v, Class<?> i, String name) {
     try {
-      System.out.println(v.getClass().getDeclaredMethod(name, i));
       return v.getClass().getDeclaredMethod(name, i);
-    } catch (NoSuchMethodException | SecurityException e) {
-      try {
-        return ASTVisitor.class.getMethod(name, Object.class);
-      } catch (NoSuchMethodException | SecurityException e1) {
-        assert false : "Should not reach";
-        return null;
-      }
+    } catch (@SuppressWarnings("unused") NoSuchMethodException | SecurityException e) {
+      return null;
     }
   }
 }
--- revision/src/test/java/org/spartan/fajita/revision/examples/usage/datalog/RunDatalogProgram.java
+++ revision/src/test/java/org/spartan/fajita/revision/examples/usage/datalog/RunDatalogProgram.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.spartan.fajita.revision.export.ASTVisitor;
+import org.spartan.fajita.revision.junk.DatalogAST;
 import org.spartan.fajita.revision.junk.DatalogAST.*;
 
 import za.co.wstoop.jatalog.DatalogException;
@@ -22,37 +24,29 @@ public class RunDatalogProgram {
   public static void main(String[] args) {
     run(Ancestor.program());
   }
-  public static void run(Program p) {
+  @SuppressWarnings("unused") public static void run(Program p) {
     Jatalog j = new Jatalog();
-    for (Statement s : p.program1)
-      try {
-        System.out.println(run(j, s));
-      } catch (DatalogException e) {
-        e.printStackTrace();
-        return;
+    ASTVisitor visitor = new ASTVisitor(DatalogAST.class) {
+      public boolean visit(Fact f) throws DatalogException {
+        j.fact(f.fact, f.by);
+        System.out.println(INPUT_PREFIX + print(f));
+        return true;
       }
-  }
-  private static String run(Jatalog j, Statement s) throws DatalogException {
-    if (s instanceof Fact)
-      return run(j, (Fact) s);
-    if (s instanceof Rule)
-      return run(j, (Rule) s);
-    return run(j, (Query) s);
-  }
-  private static String run(Jatalog j, Fact f) throws DatalogException {
-    j.fact(f.fact, f.by);
-    return INPUT_PREFIX + print(f);
-  }
-  private static String run(Jatalog j, Rule r) throws DatalogException {
-    List<Expr> $ = new LinkedList<>();
-    $.addAll(Arrays.stream(r.rule1).map(e -> Expr.expr(e.is, e.by)).collect(Collectors.toList()));
-    j.rule(Expr.expr(r.rule, r.by), $.toArray(new Expr[$.size()]));
-    return INPUT_PREFIX + print(r);
-  }
-  private static String run(Jatalog j, Query q) throws DatalogException {
-    List<Map<String, String>> $ = new LinkedList<>();
-    for (Map<String, String> x : j.query(Expr.expr(q.query, q.by)))
-      $.add(new HashMap<>(x));
-    return INPUT_PREFIX + print(q) + "\n" + OUTPUT_PREFIX + $;
+      public boolean visit(Rule r) throws DatalogException {
+        List<Expr> $ = new LinkedList<>();
+        $.addAll(Arrays.stream(r.rule1).map(e -> Expr.expr(e.is, e.by)).collect(Collectors.toList()));
+        j.rule(Expr.expr(r.rule, r.by), $.toArray(new Expr[$.size()]));
+        System.out.println(INPUT_PREFIX + print(r));
+        return true;
+      }
+      public boolean visit(Query q) throws DatalogException {
+        List<Map<String, String>> $ = new LinkedList<>();
+        for (Map<String, String> x : j.query(Expr.expr(q.query, q.by)))
+          $.add(new HashMap<>(x));
+        System.out.println(INPUT_PREFIX + print(q) + "\n" + OUTPUT_PREFIX + $);
+        return true;
+      }
+    };
+    visitor.startVisit(p);
   }
 }
