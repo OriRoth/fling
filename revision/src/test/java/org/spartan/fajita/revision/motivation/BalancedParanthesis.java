4bfc640129c1be1d15a58838773f9694c172d1f0
Ori Roth <ori.rothh@gmail.com>
2018-02-22 19:31:11 +0200

    sync

--- revision/src/test/java/org/spartan/fajita/revision/motivation/BalancedParanthesis.java
+++ revision/src/test/java/org/spartan/fajita/revision/motivation/BalancedParanthesis.java
@@ -3,8 +3,76 @@ package org.spartan.fajita.revision.motivation;
 /**
  * S ::= l S r S | ε
  */
-public class BalancedParanthesis {
-  public interface x {
-    
+@SuppressWarnings("unused") public class BalancedParanthesis {
+  public static void main(String[] args) {
+    l().r().$().α();
+    l().l().r().l().r().r().$().α();
+    l().l().r().l().l().l().r().r().r().r().$().α();
+    // l().l().r().$();
+    // l().l().r().r().r();
+    // l().l().r().l().l().r().r().r().r();
+  }
+
+  public interface $ {
+    α $();
+  }
+
+  public interface α {
+    void α();
+  }
+
+  private interface χ {
+    //
+  }
+
+  public interface Ϟ0<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> {
+    Ϟ2<χ, γ_2_4_6<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2>, χ, S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1> l();
+    S_$_0 $();
+  }
+
+  public interface Ϟ1<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> {
+    α $();
+  }
+
+  public interface Ϟ2<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> {
+    Ϟ3<χ, γ_3_5_7<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2>, χ, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> l();
+    S_r_0 r();
+  }
+
+  public interface Ϟ3<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> {
+    Ϟ3<χ, γ_3_5_7<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2>, χ, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> l();
+    S_r_0 r();
+  }
+
+  public interface Ϟ4<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> {
+    Ϟ6<χ, χ, S_$_2, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> r();
+  }
+
+  public interface Ϟ5<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> {
+    Ϟ7<χ, S_r_2, χ, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> r();
+  }
+
+  public interface Ϟ6<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> {
+    Ϟ2<χ, γ_2_4_6<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2>, χ, S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1> l();
+    S_$_2 $();
+  }
+
+  public interface Ϟ7<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> {
+    Ϟ3<χ, γ_3_5_7<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2>, χ, S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1> l();
+    S_r_0 r();
+  }
+
+  public interface γ_2_4_6<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> {
+    Ϟ2<χ, γ_2_4_6<χ, χ, S_$_0, χ, χ, χ, χ, γ_2_4_6<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2>, χ>, χ, χ, χ, χ, χ, χ, χ> l();
+    S_$_0 $();
+  }
+
+  public interface γ_3_5_7<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2> {
+    Ϟ3<χ, γ_3_5_7<χ, S_r_0, χ, χ, χ, χ, χ, γ_3_5_7<S_l_0, S_r_0, S_$_0, S_l_1, S_r_1, S_$_1, S_l_2, S_r_2, S_$_2>, χ>, χ, χ, S_r_0, χ, χ, χ, χ> l();
+    S_r_0 r();
+  }
+
+  public static Ϟ2<χ, γ_2_4_6<χ, χ, α, χ, χ, χ, χ, χ, χ>, χ, χ, χ, α, χ, χ, χ> l() {
+    return null;
   }
 }
