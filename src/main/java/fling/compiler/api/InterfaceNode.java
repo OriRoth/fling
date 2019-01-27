d1eadbff44853e470f4796aad141318d1319046b
Ori Roth <ori.rothh@gmail.com>
2019-01-27 21:00:23 +0200

    cleanup

--- src/main/java/fling/compiler/api/InterfaceNode.java
+++ src/main/java/fling/compiler/api/InterfaceNode.java
@@ -4,8 +4,8 @@ import java.util.Collections;
 import java.util.List;
 
 public class InterfaceNode<T, D, N> {
-  @SuppressWarnings("rawtypes") /* private */ static final InterfaceNode TOP = new InterfaceNode();
-  @SuppressWarnings("rawtypes") /* private */ static final InterfaceNode BOT = new InterfaceNode();
+  @SuppressWarnings("rawtypes") private static final InterfaceNode TOP = new InterfaceNode();
+  @SuppressWarnings("rawtypes") private static final InterfaceNode BOT = new InterfaceNode();
   public final N declaration;
   public final List<AbstractMethodNode<T, D>> methods;
 
