071298cab258a55da29cda76fed1e5b5b4029402
Ori Roth <ori.rothh@gmail.com>
2019-04-05 10:39:14 +0300

    output sources are formatted

--- src/test/java/fling/testutil/CompilationScript.java
+++ src/test/java/fling/testutil/CompilationScript.java
@@ -10,6 +10,9 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import com.google.googlejavaformat.java.Formatter;
+import com.google.googlejavaformat.java.FormatterException;
+
 import fling.languages.AnBn;
 import fling.languages.BalancedParentheses;
 import fling.languages.ExtendedBalancedParentheses;
@@ -31,18 +34,19 @@ public class CompilationScript {
   }
   private static final String PATH = "./src/test/java/fling/generated/";
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, FormatterException {
     System.out.println("project path: " + PATH);
     Path outputFolder = Paths.get(PATH);
     if (!Files.exists(outputFolder)) {
       Files.createDirectory(outputFolder);
       System.out.println("directory " + PATH + " created successfully");
     }
+    Formatter formatter = new Formatter();
     for (Entry<String, String> file : files.entrySet()) {
       Path filePath = Paths.get(PATH + file.getKey() + ".java");
       if (Files.exists(filePath))
         Files.delete(filePath);
-      Files.write(filePath, Collections.singleton(file.getValue()), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+      Files.write(filePath, Collections.singleton(formatter.formatSource(file.getValue())), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
       System.out.println("file " + file.getKey() + ".java written successfully.");
     }
   }
