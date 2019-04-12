790d4a63ad1ac2f215b434d0b655a4cad0cbf544
Yossi Gil <yogi@cs.technion.ac.il>
2019-04-12 09:20:59 +0300

    document datalong

--- src/test/java/fling/examples/languages/Datalog.java
+++ src/test/java/fling/examples/languages/Datalog.java
@@ -18,23 +18,30 @@ public class Datalog {
     infer, fact, query, of, and, when, always, v, l
   }
 
-  /** Set of non-terminals, i.e., abstract concepts of fluent API */
+  /**
+   * Set of non-terminals, i.e., abstract concepts of fluent API; these names
+   * will be translated into names of classes of abstract syntax tree that Fling
+   * generates, i.e., this AST will have class {@link Program} which will have a
+   * list of {@link Statement}, etc.
+   */
   public enum V implements Variable {
     Program, Statement, Rule, Query, Fact, Bodyless, WithBody, //
     RuleHead, RuleBody, FirstClause, AdditionalClause, Term
   }
 
   /**
-   * Short name of String.class, used to specify the type of parameters to
-   * fluent API methods in grammar specification
+   * Short name of {@link String}.class, used to specify the type of parameters
+   * to fluent API methods in grammar specification
    */
   private static final Class<String> S = String.class;
   public static final BNF bnf = bnf(). //
       start(Program). // This is the start symbol
       derive(Program).to(oneOrMore(Statement)). // Program ::= Statement*
-      specialize(Statement).into(Fact, Rule, Query). // Statement ::= Fact | Rule | Query
-      // also, defines
-      derive(Fact).to(fact.with(S), of.many(S)). //
+      specialize(Statement).into(Fact, Rule, Query).
+      /* Defines the rule Statement ::= Fact |Rule | Query, but also defines
+       * that classes {@link Fact}, {@link Rule} and {@link Query} extend class
+       * {@link Statement} */
+      derive(Fact).to(fact.with(S), of.many(S)). // Fact ::= fact(S*) of(S*)
       derive(Query).to(query.with(S), of.many(Term)). //
       specialize(Rule).into(Bodyless, WithBody). //
       derive(Bodyless).to(always.with(S), of.many(Term)). //
