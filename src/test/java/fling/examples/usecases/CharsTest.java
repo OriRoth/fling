3852049615e37fa5f870ef7f7465412aef013de4
Yossi Gil <yogi@cs.technion.ac.il>
2020-02-26 14:49:41 +0200

    Chars

---
+++ src/test/java/fling/examples/usecases/CharsTest.java
@@ -0,0 +1,11 @@
+package fling.examples.usecases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CharsTest {
+  @Test void test() {
+    fail("Not yet implemented");
+  }
+}
