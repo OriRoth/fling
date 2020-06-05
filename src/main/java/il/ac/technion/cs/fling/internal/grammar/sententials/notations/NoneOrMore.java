4055cd59291f7641840bd0b9ecb8bfca057adb97
yossigil <yossi.gil@gmail.com>
2020-06-05 23:44:39 +0300

    More functions

--- src/main/java/il/ac/technion/cs/fling/internal/grammar/sententials/notations/NoneOrMore.java
+++ src/main/java/il/ac/technion/cs/fling/internal/grammar/sententials/notations/NoneOrMore.java
@@ -13,72 +13,96 @@ import il.ac.technion.cs.fling.internal.grammar.sententials.*;
 import il.ac.technion.cs.fling.internal.grammar.types.ClassParameter;
 
 // TODO support nested notations (?).
-@JavaCompatibleNotation public class NoneOrMore implements Quantifier {
-  public final Symbol symbol;
+@JavaCompatibleNotation
+public class NoneOrMore implements Quantifier {
+	public final Symbol symbol;
 
-  public NoneOrMore(final Symbol symbol) {
-    Objects.requireNonNull(symbol);
-    assert !symbol.isNotation() : "nested notations are not supported";
-    this.symbol = symbol;
-  }
-  @Override public String name() {
-    return symbol.name() + "*";
-  }
-  @Override public Variable extend(final Namer namer, final Consumer<Variable> variableDeclaration,
-      final Consumer<DerivationRule> ruleDeclaration) {
-    final Variable head = namer.createNotationChild(symbol);
-    variableDeclaration.accept(head);
-    ruleDeclaration.accept(new DerivationRule(head, asList(//
-        new SententialForm(symbol, head), //
-        new SententialForm())));
-    return head;
-  }
-  @Override public Collection<Symbol> abbreviatedSymbols() {
-    return asList(symbol);
-  }
-  @Override public List<FieldNodeFragment> getFields(final Function<Symbol, List<FieldNodeFragment>> fieldsSolver,
-      @SuppressWarnings("unused") final Function<String, String> nameFromBaseSolver) {
-    // TODO manage inner symbol with no fields.
-    return fieldsSolver.apply(symbol).stream() //
-        .map(innerField -> new FieldNodeFragment( //
-            String.format("%s<%s>", //
-                List.class.getCanonicalName(), //
-                ClassParameter.unPrimitiveType(innerField.parameterType)), //
-            innerField.parameterName) {
-          @Override public String visitingMethod(final BiFunction<Variable, String, String> variableVisitingSolver,
-              final String accessor, final Supplier<String> variableNamesGenerator) {
-            if (!symbol.isVariable())
-              return null;
-            final String streamingVariable = variableNamesGenerator.get();
-            return String.format("%s.stream().forEach(%s->%s)", //
-                accessor, //
-                streamingVariable, //
-                variableVisitingSolver.apply(symbol.asVariable(), streamingVariable));
-          }
-        }) //
-        .collect(toList());
-  }
-  @SuppressWarnings("unused") @Override public boolean isNullable(final Function<Symbol, Boolean> nullabilitySolver) {
-    return true;
-  }
-  @Override public Set<Verb> getFirsts(final Function<Symbol, Set<Verb>> firstsSolver) {
-    return firstsSolver.apply(symbol);
-  }
-  @Override public String toString() {
-    return symbol + "*";
-  }
-  @SuppressWarnings("unchecked") public static List<List<Object>> abbreviate(final List<Object> rawNode, final int fieldCount) {
-    final List<List<Object>> $ = new ArrayList<>();
-    for (int i = 0; i < fieldCount; ++i)
-      $.add(new ArrayList<>());
-    List<Object> currentRawNode = rawNode;
-    while (!currentRawNode.isEmpty()) {
-      assert currentRawNode.size() == fieldCount + 1;
-      final List<Object> rawArguments = currentRawNode.subList(0, fieldCount);
-      for (int i = 0; i < fieldCount; ++i)
-        $.get(i).add(rawArguments.get(i));
-      currentRawNode = (List<Object>) currentRawNode.get(fieldCount);
-    }
-    return $;
-  }
+	public NoneOrMore(final Symbol symbol) {
+		Objects.requireNonNull(symbol);
+		assert !symbol.isNotation() : "nested notations are not supported";
+		this.symbol = symbol;
+	}
+
+	@Override
+	public String name() {
+		return symbol.name() + "*";
+	}
+
+	@Override
+	public Variable extend(final Namer namer, final Consumer<Variable> variableDeclaration,
+			final Consumer<DerivationRule> ruleDeclaration) {
+		final Variable head = namer.createNotationChild(symbol);
+		variableDeclaration.accept(head);
+		ruleDeclaration.accept(new DerivationRule(head, asList(//
+				new SententialForm(symbol, head), //
+				new SententialForm())));
+		return head;
+	}
+
+	@Override
+	public Collection<Symbol> abbreviatedSymbols() {
+		return asList(symbol);
+	}
+
+	@Override
+	public List<FieldNodeFragment> getFields(final Function<Symbol, List<FieldNodeFragment>> fieldsSolver,
+			@SuppressWarnings("unused") final Function<String, String> nameFromBaseSolver) {
+		// TODO manage inner symbol with no fields.
+		return fieldsSolver.apply(symbol).stream() //
+				.map(innerField -> new FieldNodeFragment( //
+						String.format("%s<%s>", //
+								List.class.getCanonicalName(), //
+								ClassParameter.unPrimitiveType(innerField.parameterType)), //
+						innerField.parameterName) {
+					@Override
+					public String visitingMethod(final BiFunction<Variable, String, String> variableVisitingSolver,
+							final String accessor, final Supplier<String> variableNamesGenerator) {
+						if (!symbol.isVariable())
+							return null;
+						final String streamingVariable = variableNamesGenerator.get();
+						return String.format("%s.stream().forEach(%s->%s)", //
+								accessor, //
+								streamingVariable, //
+								variableVisitingSolver.apply(symbol.asVariable(), streamingVariable));
+					}
+				}) //
+				.collect(toList());
+	}
+
+	@SuppressWarnings("unused")
+	@Override
+	public boolean isNullable(final Function<Symbol, Boolean> nullabilitySolver) {
+		return true;
+	}
+
+	@Override
+	public Set<Verb> getFirsts(final Function<Symbol, Set<Verb>> firstsSolver) {
+		return firstsSolver.apply(symbol);
+	}
+
+	@Override
+	public String toString() {
+		return symbol + "*";
+	}
+
+	@SuppressWarnings("unchecked")
+	public static List<List<Object>> abbreviate(final List<Object> rawNode, final int fieldCount) {
+		final List<List<Object>> $ = new ArrayList<>();
+		for (int i = 0; i < fieldCount; ++i)
+			$.add(new ArrayList<>());
+		List<Object> currentRawNode = rawNode;
+		while (!currentRawNode.isEmpty()) {
+			assert currentRawNode.size() == fieldCount + 1;
+			final List<Object> rawArguments = currentRawNode.subList(0, fieldCount);
+			for (int i = 0; i < fieldCount; ++i)
+				$.get(i).add(rawArguments.get(i));
+			currentRawNode = (List<Object>) currentRawNode.get(fieldCount);
+		}
+		return $;
+	}
+
+	@Override
+	public Symbol symbol() {
+		return symbol;
+	}
 }
