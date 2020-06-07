4dc7fb91c4e73b831ec4040983dff9f026f1a21f
Ori Roth <ori.rothh@gmail.com>
2020-06-07 18:00:55 +0300

    added run-time test

---
+++ src/test/java/il/ac/technion/cs/fling/examples/usecases/RuntimeTest.java
@@ -0,0 +1,14 @@
+package il.ac.technion.cs.fling.examples.usecases;
+
+import org.junit.Test;
+
+public class RuntimeTest {
+  @SuppressWarnings("static-method") @Test public void testAll() {
+    Datalog.main(null);
+    HTMLTable.main(null);
+    SimpleArithmeticUseCase.main(null);
+    SubFigure.main(null);
+    TableMaker.main(null);
+    TaggedBalancedParentheses.main(null);
+  }
+}
