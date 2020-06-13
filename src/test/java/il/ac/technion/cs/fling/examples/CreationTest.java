4062cf83965b638f31c8b0452802a631404db2a7
yossigil <yossi.gil@gmail.com>
2020-06-13 15:57:59 +0300

    add finals

--- src/test/java/il/ac/technion/cs/fling/examples/CreationTest.java
+++ src/test/java/il/ac/technion/cs/fling/examples/CreationTest.java
@@ -9,17 +9,17 @@ import il.ac.technion.cs.fling.examples.languages.Datalog;
 
 class CreationTest {
   @Test public void checkDatalog() {
-    Datalog x = new Datalog();
+    final Datalog x = new Datalog();
     new JavaMediator(x.BNF(), "", x.name(), x.Σ());
   }
 
   @Test public void checkBNF() {
-    BNF x = new BNF();
+    final BNF x = new BNF();
     new JavaMediator(x.BNF(), "", x.name(), x.Σ());
   }
 
   @Test public void checkBalancedParentheses() {
-    BalancedParentheses x = new BalancedParentheses();
+    final BalancedParentheses x = new BalancedParentheses();
     new JavaMediator(x.BNF(), "", x.name(), x.Σ());
   }
 
