b61f5388d8d1db8c9f45dbb495799734b83f7ef5
yossigil <yossi.gil@gmail.com>
2020-06-22 19:45:37 +0300

    fewer errors

--- src/main/java/il/ac/technion/cs/fling/adapters/CSharpGenerator.java
+++ src/main/java/il/ac/technion/cs/fling/adapters/CSharpGenerator.java
@@ -1,13 +1,12 @@
 package il.ac.technion.cs.fling.adapters;
 import static java.util.stream.Collectors.joining;
-import java.util.Set;
 import java.util.stream.Collectors;
 import il.ac.technion.cs.fling.internal.compiler.Linker;
+import il.ac.technion.cs.fling.internal.compiler.api.dom.Method;
 import il.ac.technion.cs.fling.internal.compiler.api.dom.Model;
 import il.ac.technion.cs.fling.internal.compiler.api.dom.Type;
 import il.ac.technion.cs.fling.internal.grammar.rules.Constants;
 import il.ac.technion.cs.fling.internal.grammar.rules.Named;
-import il.ac.technion.cs.fling.internal.grammar.rules.Word;
 /** C# API adapter.
  *
  * @author Ori Roth */
@@ -15,21 +14,21 @@ public class CSharpGenerator extends CLikeGenerator {
   public CSharpGenerator(final Linker namer) {
     super(namer);
   }
-  @Override public String render(final MethodSignature s, final Type.Grounded returnType) {
+  public String render(final Method m) {
     return String.format("public %s %s(%s){return new %s();}", //
-        render(returnType), //
-        s.name.name(), //
-        printParametersList(s), //
-        render(returnType));
+        render(m.type), //
+        m.name.name(), //
+        printParametersList(m), //
+        render(m.type));
   }
   @Override void visit(final Type t) {
     final String printTypeName = fullName(t);
-    return s.parameters.isEmpty() ? String.format("public class %s", printTypeName)
+    return t.parameters.isEmpty() ? String.format("public class %s", printTypeName)
         : String.format("public class %s<%s>%s", //
             printTypeName, //
-            s.parameters().map(this::typeVariableName) //
+            t.parameters().map(this::typeVariableName) //
                 .collect(Collectors.joining(",")),
-            s.parameters().map(this::typeVariableName) //
+            t.parameters().map(this::typeVariableName) //
                 .map(n -> "where " + n + ":new()") //
                 .collect(Collectors.joining("")) //
         );
@@ -52,22 +51,10 @@ public class CSharpGenerator extends CLikeGenerator {
   public String typeVariableName(final Named typeVariable) {
     return "_" + typeVariable.name();
   }
-  @Override protected String comment(String comment) {
-    return String.format("/* %s */", comment);
-  }
   @Override String render(final Model m) {
     return String.format("%s%s", //
         m.types().map(this::render).collect(joining()), //
         m.starts().map(this::render).collect(joining())) //
         .replace("$", "τ");
-  }
-  @Override String render(final Named q, final Word<Named> α, final Set<Named> legalJumps) {
-    final String qn = q.name();
-    // TODO: manage this HACK
-    return α == null ? qn.contains("_") ? qn : typeVariableName(q)
-        : String.format("%s_%s%s", //
-            q.name(), //
-            α.stream().map(Named::name).collect(Collectors.joining()), //
-            legalJumps == null ? "" : "_" + legalJumps.stream().map(Named::name).collect(Collectors.joining()));
   }
 }
